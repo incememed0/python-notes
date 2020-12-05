dd686a221f363ffa8d15f2cc846848c634d851b6
incememed0 <mek.muh.ercan@protonmail.com>
2020-12-05 14:52:08 +0300

    5.gün

--- 4-veri_tipi_donusumleri.py
+++ 4-veri_tipi_donusumleri.py
@@ -1,3 +1,37 @@
 # author: Ercan Atar
 # linkedin.com/in/ercanatar/
 #######################################################
+# Tam sayıyı ondalık sayıya çevirme.
+a=42
+a=float(a)
+print(a)
+
+sayi1=int(12)
+print(sayi1)
+# Ondalık sayıyı tam sayıya çevirme.
+b=4.12345
+b=int(b)
+print(b)
+
+sayi2=int(23.12342342)
+print(sayi2)
+# Sayıları string ifadeye çevirme.
+kelime=str(1234.123)
+print(kelime)
+
+# kelime uzunluğu
+print(len(kelime))
+
+# \n ---> alt satıra geç
+# \t ---> 4 boşluk bırak
+
+# değişkenin hangi veri tipine sahip olduğunun çıktısı.
+print(type(kelime))
+print(type(a))
+print(type(b))
+print("---\t---\t---\t---")
+# Çoğunlukla biz bu veri tipi dönüşümlerini int --> str şeklinde kullanıyoruz.
+a=12
+print(type(a),a)
+a=str(a)
+print(type(a),a)
---
+++ 5-print_formatlari.py
@@ -0,0 +1,22 @@
+# author: Ercan Atar
+# linkedin.com/in/ercanatar/
+#######################################################
+# Print'i efektif bir şekilde kullanarak kodlarını düzenli hale getirebilirsin.
+print('tek tırnakla')
+print("çift tırnakla")
+print('''üç
+ tırnakla
+bile yazarsın.''')
+print("""böyle
+bile
+yazarsın.""")
+print("-----------------")
+print("kelime",2,4.123)
+# sep parametresi sayesinde değerlerin arasına nokta konuyor.
+print(3,4,5,6,7,8,9,sep = ".")
+# Değerlerin arasında "/" sembolü yerleştiriliyor.
+print("06","04","2015",sep = "/")
+print("tryhackme","hackthebox","priviahub",sep = "\n")
+# Varsayılan olarak karakterlerin arasına boşluk konuluyor.
+print(*"Python")
+print(*"TBMM",sep =".")
