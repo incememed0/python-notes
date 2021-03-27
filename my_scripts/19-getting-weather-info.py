aabe4158c64436646947e1b338d3ce465c4a07ec
Ercan Atar <mek.muh.ercan@protonmail.com>
2021-03-27 14:44:48 +0300

    düzenlendi

--- my_scripts/19-getting-weather-info.py
+++ my_scripts/19-getting-weather-info.py
@@ -6,7 +6,7 @@
 import requests
 from pprint import pprint
 
-API_Key="8c467e3e443c968139157c9bcc10dafc"
+API_Key=""
 city=input("enter a city: ")
 base_url="http://api.openweathermap.org/data/2.5/weather?appid="+API_Key+"&q="+city
 weather_data=requests.get(base_url).json()
