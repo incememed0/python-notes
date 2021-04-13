dc4866b4630c4c6092c1e10ad19ba91bfcc04e3c
Ercan Atar <mek.muh.ercan@protonmail.com>
2021-04-13 22:07:02 +0300

    yeni çalışmalarım

--- my_scripts/32-beautifulsoup-html.py
+++ my_scripts/32-beautifulsoup-html.py
@@ -1,6 +1,8 @@
 # author: Ercan Atar
 # linkedin.com/in/ercanatar/
 ################################################
+# dökümantasyon: https://www.crummy.com/software/BeautifulSoup/bs4/doc/
+
 from bs4 import BeautifulSoup
 
 html_doc="""
@@ -28,6 +30,16 @@ html_doc="""
     <li>menü.3</li>
 </ul>
     </div>
+    
+    </div>
+    <div class="grup3">
+    <h2>django</h2>
+<ul>
+    <li>menü.11</li>
+    <li>menü.22</li>
+    <li>menü.33</li>
+</ul>
+    </div>
 </body>
 </html>
 """
@@ -51,6 +63,8 @@ soup=BeautifulSoup(html_doc,'html.parser')
 #result=soup.div
 #result=soup.find_all('div')[1].ul.find_all('li')
 
-result=soup.div.findChildren()
+#result=soup.div.findChildren()
+
+result=soup.div.findNextSibling().findNextSibling().findPreviousSibling()
 
 print(result)
